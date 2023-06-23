cb309d2b6404f53137ba53e37360f744c788e0df
Iryna Brusentsova <bio200480@gmail.com>
2023-06-23 21:46:58 +0300

    Add anination to project

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,18 @@ document.querySelector('.burger').addEventListener('click', function(){
 	this.classList.toggle('active');
 	document.querySelector('.nav').classList.toggle('open');
 });
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
