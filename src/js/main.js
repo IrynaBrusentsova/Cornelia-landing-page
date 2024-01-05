b0ad2cf5ccb6b071edb98c71c6daacf68629c92b
Iryna Brusentsova <bio200480@gmail.com>
2024-01-05 10:06:42 +0200

    Add function

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,30 @@
-'use strict';
+"use strict";
 
-document.querySelector('.burger').addEventListener('click', function(){
-	this.classList.toggle('active');
-	document.querySelector('.nav').classList.toggle('open');
+document.querySelector(".burger").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".nav").classList.toggle("open");
 });
 
-
 function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-        change.target.classList.add('element-show');
-      }
-    });
-  }
-  let options = { threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.element-animation');
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+for (let elm of elements) {
+  observer.observe(elm);
+}
 
- document.getElementById('btnChoose').onclick = function(){
-  window.location.href = '#banner';
- };
- 
-  console.log(document.getElementById('btnChoose'));
+document.getElementById("btnChoose").onclick = function () {
+  window.location.href = "#banner";
+};
 
-  console.log(document.getElementById('btnEachDetail'));
+console.log(document.getElementById("btnChoose"));
 
-  console.log(document.getElementById('bthUnicornExit'));
+console.log(document.getElementById("btnEachDetail"));
+
+console.log(document.getElementById("bthUnicornExit"));
